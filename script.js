e3a5d12a7b4034077098e8f400d6a0c83f0b2f9b
nahil140 <nabekaddour@eleve.st-charles.eu>
2025-11-04 15:30:10 +0000

    feat: Maitrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Laboratoire prêt !")
+const nom = "Nahil";
+let age = 16;
+console.log (nom)
+console.log (age)
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "12345";
+if ("Comparaison entre motDePasseUtilisateur et motDePasseAttendu") {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
